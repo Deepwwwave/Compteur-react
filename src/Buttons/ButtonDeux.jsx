e52c76a1fa763fd8df69c2bb6685444c89822e5b
deepwwwave <deepwwwave@gmail.com>
2022-04-12 08:11:06 +0200

    first

---
+++ src/Buttons/ButtonDeux.jsx
@@ -0,0 +1,60 @@
+import React, { Component } from "react";
+import './button.css'
+
+export default class ButtonDeux extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      count: 0,
+      toggle: false,
+      timer: null,
+      btnReset: false,
+    };
+  }
+
+  start = () => {
+    this.setState({
+      count: this.state.count + 2,
+    });
+  };
+
+  chrono = () => {
+    if (this.state.toggle === false) {
+      this.setState({
+        timer: setInterval(this.start, 1000),
+        btnReset: true,
+      });
+    }
+    this.setState({
+      toggle: true,
+    });
+  };
+
+  stop = () => {
+    clearInterval(this.state.timer);
+    this.setState({
+      toggle: false,
+    });
+  };
+  reset = () => {
+    clearInterval(this.state.timer);
+    this.setState({
+      toggle: false,
+      count: 0,
+      btnReset: false,
+    });
+  };
+  
+  render() {
+    console.log(this.state.btnReset)
+    console.log(this.state.toggle);
+    return (
+      <>
+        <button onClick={this.chrono}>Start Count +1</button>
+        <button onClick={this.stop}>Stop</button>
+        <button onClick={this.reset} className={this.state.btnReset ? "display" : "displayNone"}>Reset</button>
+        <p>{`${this.state.count}`}</p>
+      </>
+    );
+  }
+}
---
+++ src/Home/Home.jsx
@@ -0,0 +1,15 @@
+import React, { Component } from 'react'
+import ButtonUn from '../Buttons/ButtonUn'
+import ButtonDeux from '../Buttons/ButtonDeux'
+import './home.css'
+
+export default class Home extends Component {
+  render() {
+    return (
+      <>
+      <ButtonUn></ButtonUn>
+      <ButtonDeux></ButtonDeux>
+      </>
+    )
+  }
+}
