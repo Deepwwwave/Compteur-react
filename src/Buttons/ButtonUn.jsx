0b6de3df7e9d3360084863294ddec906dc5c4962
deepwwwave <deepwwwave@gmail.com>
2022-04-12 08:19:54 +0200

    ajout commentaire btn1

--- src/Buttons/ButtonUn.jsx
+++ src/Buttons/ButtonUn.jsx
@@ -5,19 +5,25 @@ export default class ButtonUn extends Component {
   constructor(props) {
     super(props);
     this.state = {
+      //compteur
       count: 0,
+      // chrono activ ou non
       toggle: false,
+      // setInterval
       timer: null,
+      // btn reset display ou non
       btnReset: false,
     };
   }
 
+    // start incr count
   start = () => {
     this.setState({
       count: this.state.count + 1,
     });
   };
 
+  // interval methode start
   chrono = () => {
     if (this.state.toggle === false) {
       this.setState({
@@ -30,12 +36,15 @@ export default class ButtonUn extends Component {
     });
   };
 
+  // stop > clear Interval > 
   stop = () => {
     clearInterval(this.state.timer);
     this.setState({
       toggle: false,
     });
   };
+
+  // reset states + clearInterval
   reset = () => {
     clearInterval(this.state.timer);
     this.setState({
@@ -45,6 +54,7 @@ export default class ButtonUn extends Component {
     });
   };
   
+
   render() {
     console.log(this.state.btnReset)
     console.log(this.state.toggle);
